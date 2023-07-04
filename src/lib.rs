0544bf336dde13016024c3be378b1f020165a8b1
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-07-04 19:07:58 +0200

    feat(metrics): prometheus

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,11 @@ pub mod db;
 pub mod routes;
 
 use crate::db::{db_connect, PoolDb};
-use crate::routes::fetch_cities::fetch_cities_handler;
-use crate::routes::{create_city::create_city_handler, health::health_handler};
+use crate::routes::{
+    create_city::create_city_handler, fetch_cities::fetch_cities_handler, health::health_handler,
+};
 use actix_web::{middleware, web, App, HttpServer};
+use actix_web_prom::{PrometheusMetrics, PrometheusMetricsBuilder};
 use log::info;
 use std::env;
 use std::io::{Error, ErrorKind};
@@ -18,6 +20,7 @@ pub async fn create_server() -> std::io::Result<()> {
     pretty_env_logger::init_timed();
 
     let pool = db_connect().await?;
+    let prometheus = prometheus_config();
 
     let addr = get_env("CITY_API_ADDR").unwrap_or("127.0.0.1".to_owned());
     let port = get_env("CITY_API_PORT").unwrap_or("2022".to_owned());
@@ -25,6 +28,7 @@ pub async fn create_server() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         App::new()
+            .wrap(prometheus.clone())
             .wrap(middleware::Compress::default())
             .app_data(web::Data::new(AppState { db: pool.clone() }))
             .service(health_handler)
@@ -38,4 +42,11 @@ pub async fn create_server() -> std::io::Result<()> {
 
 pub fn get_env(key: &str) -> Result<String, Error> {
     env::var(key).map_err(|_| Error::new(ErrorKind::NotFound, format!("'{}' not found", key)))
+}
+
+pub fn prometheus_config() -> PrometheusMetrics {
+    PrometheusMetricsBuilder::new("city_api")
+        .endpoint("/metrics")
+        .build()
+        .unwrap()
 }
